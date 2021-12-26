d7e5b84784dfd09c59124bfdbd98fcfc90e98395
ykent <ppscam225@gmail.com>
2021-12-26 18:50:05 +0900

    あとすこし

--- kuizy.js
+++ kuizy.js
@@ -1,7 +1,6 @@
 'use strict';
 
-let quiz_div_wrapper = document.getElementById('quiz_div_wrapper');
-
+//選択肢（それぞれの0番目のが正解になっている）
 let optionTextArray = [
   ['たかなわ','こうわ','たかわ'],
   ['かめいど','かめど','かめと'],
@@ -13,41 +12,106 @@ let optionTextArray = [
   ['おかちまち','みとちょう','ごしろちょう'],
   ['ししぼね','しこね','ろっこつ'],
   ['こぐれ','こしゃく','こばく'],
+];
+
+//回答ボックスのメッセージ
+let answerBoxArray = [
+  '正解は「たかなわ」です！',
+  '正解は「かめいど」です！',
+  '正解は「こうじまち」です！',
+  '正解は「おなりもん」です！',
+  '正解は「とどろき」です！',
+  '正解は「しゃくじい」です！',
+  '正解は「ぞうしき」です！',
+  '正解は「おかちまち」です！',
+  '江戸川区にあります。',
+  '正解は「こぐれ」です！',
+];
+
+let optionHtml = [
+
+  ' `<li class="option" id="correctAnswerQ${i + 1}" onclick="show_answer_correct(${i + 1})">${optionTextArray[i][0]}</li>` ',
+  ' `<li class="option" id="wrongAnswerQ${i + 1}_1" onclick="show_answer_wrong_1(${i + 1})">${optionTextArray[i][1]}</li>` ',
+  ' `<li class="option" id="wrongAnswerQ${i + 1}_2" onclick="show_answer_wrong_2(${i + 1})">${optionTextArray[i][2]}</li>` ',
 
 ]
 
+//ソートされた配列
+let a = [0,1,2];
+let j = 0;
+let r = 0;
+let tmp = 0;
+
+// for(j = a.length -1;j>0;j--){
+//   //乱数生成を使ってランダムに取り出す値を決める
+//   r = Math.floor(Math.random()*(j+1));
+//   //取り出した値と箱の外の先頭の値を交換する
+//   tmp = a[j];
+//   a[j] = a[r];
+//   a[r] = tmp;
+// }
+
+
+
+
+
+//下のfor文の中に書いたhtmlをぶち込む先
+let quizDivWrapper = document.getElementById('quizDivWrapper');
+
+//htmlへ記述したいことをfor文で繰り返し生成している。
 for (let i = 0 ; i<10 ; i++){
   let articles = 
     '<div class="quiz_div"> '
-
+      //「この地名はなんて読む？」と漢字の画像
       + `<h2 class="question">${i + 1}.この地名はなんて読む？</h2>`
       + '<div class="question-image-container">'    
       +      `<img src="/images/${i + 1}.png" alt="" class="question_image">`
       + '</div>'
 
-      + `<ul class="option_list" id="optionList${i + 1}">`
-      + `<li class="option" id="correctAnswerQ${i + 1}" onclick="show_answer_correct(${i + 1})">${optionTextArray[i][0]}</li>`
-      + `<li class="option" id="wrongAnswerQ${i + 1}_1" onclick="show_answer_wrong_1(${i + 1})">${optionTextArray[i][1]}</li>`
-      +  `<li class="option" id="wrongAnswerQ${i + 1}_2" onclick="show_answer_wrong_2(${i + 1})">${optionTextArray[i][2]}</li>`
-      + '</+ul>'
+      //選択肢 一番上（配列の0番目）が正解の選択肢になっている。
 
+      for(j = a.length -1;j>0;j--){
+        //乱数生成を使ってランダムに取り出す値を決める
+        r = Math.floor(Math.random()*(j+1));
+        //取り出した値と箱の外の先頭の値を交換する
+        tmp = a[j];
+        a[j] = a[r];
+        a[r] = tmp;
+      }
+
+      + `<ul class="option_list" id="optionList${i + 1}">`
+
+        // + `<li class="option" id="correctAnswerQ${i + 1}" onclick="show_answer_correct(${i + 1})">${optionTextArray[i][0]}</li>`
+  
+        // + `<li class="option" id="wrongAnswerQ${i + 1}_1" onclick="show_answer_wrong_1(${i + 1})">${optionTextArray[i][1]}</li>`
+  
+        // +  `<li class="option" id="wrongAnswerQ${i + 1}_2" onclick="show_answer_wrong_2(${i + 1})">${optionTextArray[i][2]}</li>`
+
+
+        +`${optionHtml[a[0]]}`
+        +`${optionHtml[a[1]]}`
+        +`${optionHtml[a[2]]}`
+
+
+        + '</+ul>'
+  
+
+      //正解の時の回答ボックスのメッセージ
       + `<div class="hidden_message_correct" id="correctPopQ${i + 1}">`
       +  '<p class="correct">正解！</p>'
-      +  '<p">正解は「たかなわ」です！</p>'
+      +  `<p>${answerBoxArray[i]}</p>`
       + '</div>'
-
+      //不正解の時の回答ボックスのメッセージ
       + `<div class="hidden_message_wrong" id="wrongPopQ${i + 1}">`
       +   '<p class="wrong">不正解！</p>'
-      +   '<p>正解は「たかなわ」です！</p>'
+      +   `<p>${answerBoxArray[i]}</p>`
       + '</div>'
       
     +'</div>'
 
-  quiz_div_wrapper.insertAdjacentHTML('beforeend',articles)
-
+  //↓ 上でつくったquiz_divというidがつけられたdivをkuizy.htmlのquizDivWrapperというidがつけられたdivのなかにぶちこんでいる。beforeend！！
+  quizDivWrapper.insertAdjacentHTML('beforeend',articles)
 }
-
-
 
 // let correctAnswerQ1 = document.getElementById('correctAnswerQ1');
 // let wrongAnswerQ1_1 = document.getElementById('wrongAnswerQ1_1');
@@ -55,9 +119,9 @@ for (let i = 0 ; i<10 ; i++){
 // let correctPopQ1 = document.getElementById('correctPopQ1');
 // let wrongPopQ1 = document.getElementById('wrongPopQ1');
 // let optionList = document.getElementById('optionList');
+// ＜！！！＞ ↑ idは1つのhtmlで同じのは一回しか使えないので、上のfor文の`<ul class="option_list" id="optionList${i + 1}">`のようにテンプレートリテラルを使ってあげて繰り返しのそれぞれについて、idが違う値になるようにしないといけない。
 
-
-
+//正解のボタン押したときの表示変更や回答ボックスの表示
 function show_answer_correct(quiz_num) {
   let correctAnswer = document.getElementById(`correctAnswerQ${quiz_num}`)
   let correctPop = document.getElementById(`correctPopQ${quiz_num}`)
@@ -68,6 +132,7 @@ function show_answer_correct(quiz_num) {
   let optionList = document.getElementById(`optionList${quiz_num}`);
   optionList.style.pointerEvents = "none"
 }
+//1つ目の不正解のボタン押したときの表示変更や回答ボックスの表示
 function show_answer_wrong_1(quiz_num) {
   let correctAnswer = document.getElementById(`correctAnswerQ${quiz_num}`)
   let wrongAnswer = document.getElementById(`wrongAnswerQ${quiz_num}_1`)
@@ -80,7 +145,7 @@ function show_answer_wrong_1(quiz_num) {
   let optionList = document.getElementById(`optionList${quiz_num}`);
   optionList.style.pointerEvents = "none";
 }
-
+//2つ目の不正解のボタン押したときの表示変更や回答ボックスの表示
 function show_answer_wrong_2(quiz_num) {
   let correctAnswer = document.getElementById(`correctAnswerQ${quiz_num}`)
   let wrongAnswer = document.getElementById(`wrongAnswerQ${quiz_num}_2`)
@@ -93,6 +158,3 @@ function show_answer_wrong_2(quiz_num) {
   let optionList = document.getElementById(`optionList${quiz_num}`);
   optionList.style.pointerEvents = "none";
 }
-
-
-
