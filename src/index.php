ff51a8c8aa1a6940a5acad7f38d8a0c01a0e30d9
{Y-K-15} <ppscam225@gmail.com>
2022-07-08 14:53:59 +0900

    localhostに接続できた。” git commit -mlocalhostに接続できた
    git add .

--- src/index.php
+++ src/index.php
@@ -1,7 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
 
-$numbers = [1,2,3,4,5];
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>クイズ</title>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/reset.css">
+</head>
 
-foreach ($numbers as $number) {
-    echo $number . PHP_EOL;
-}
+<body class="body">
+    <header class="header">
+        <img src="./images/kuizy_logo.jpg" alt="" class="kuizy_logo">
+        <div class="header_right_side">
+            <p>クイズ・診断を作成</p>
+            <div><img src="" alt=""></div>
+            <p>検索</p>
+        </div>
+    </header>
+    <main>
+        <div class="main_container">
+            <h1 class="question_title">
+                ガチで東京の人しか解けない！ #東京の難読地名クイズ
+            </h1>
+            <div class="main_inner" id="quizDivWrapper">
+            </div>
+        </div>
+        </div>
+    </main>
+    <script src="js/kuizy.js"></script>
+</body>
+
+</html>
