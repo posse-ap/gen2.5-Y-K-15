5bd9de3ed1e2fc872ac13282166e9466c5c5e2ac
{Y-K-15} <ppscam225@gmail.com>
2022-09-20 22:55:15 +0900

    9/20

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -13,21 +13,15 @@ CREATE SCHEMA posse;
 
 USE posse;
 
-
 DROP TABLE IF EXISTS big_questions;
 
-CREATE TABLE big_questions(
-  id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-);
+CREATE TABLE
+    big_questions (
+      id INT NOT NULL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+    );
 INSERT INTO
-  big_questions(id, name);
-VALUES
-  (1, "東京の難読地名クイズ");
+    big_questions(id, name) VALUES (1, "東京の難読地名クイズ");
 
 INSERT INTO
-  big_questions(id, name);
-VALUES
-  (2, "広島の難読地名クイズ");
-
-
+    big_questions(id, name) VALUES (2, "広島の難読地名クイズ");
