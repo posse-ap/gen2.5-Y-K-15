87dab748b7d6e0e182943729056c52fe067b97f5
{Y-K-15} <ppscam225@gmail.com>
2022-10-04 09:59:41 +0900

    文字化け以外東京の難読地名は完成した。

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -27,95 +27,97 @@ DROP TABLE IF EXISTS questions;
 CREATE TABLE
     questions (
       id INT NOT NULL PRIMARY KEY,
-      big_questions_id INT NOT NULL PRIMARY KEY,
+      big_question_id INT NOT NULL,
       image VARCHAR(255) NOT NULL
     );
 
 INSERT INTO questions
 SET
   id = 1,
-  big_questions_id = 1,
+  big_question_id = 1,
   image = "takanawa.png";
 
 INSERT INTO questions
 SET
   id = 2,
-  big_questions_id = 2,
+  big_question_id = 1,
   image = "kameido.png";
 
 INSERT INTO questions
 SET
   id = 3,
-  big_questions_id = 3,
+  big_question_id = 2,
   image = "mukainada.jpg";
+
+DROP TABLE IF EXISTS choices;
 
 CREATE TABLE
     choices (
       id INT NOT NULL PRIMARY KEY,
-      questions_id INT NOT NULL PRIMARY KEY,
-      image VARCHAR(255) NOT NULL,
+      question_id INT NOT NULL,
+      name VARCHAR(255) NOT NULL,
       valid VARCHAR(255) NOT NULL
     );
 
 INSERT INTO choices
 SET
   id = 1,
-  questions_id = 1,
-  name = "たかなわ",
+  question_id = 1,
+  name = "takanawa",
   valid = 1;
 
 INSERT INTO choices
 SET
   id = 2,
-  questions_id = 1,
-  name = "たかわ",
+  question_id = 1,
+  name = "takawa",
   valid = 0;
 
 INSERT INTO choices
 SET
   id = 3,
-  questions_id = 1,
-  name = "こうわ",
+  question_id = 1,
+  name = "kouwa",
   valid = 0;
 
 INSERT INTO choices
 SET
   id = 4,
-  questions_id = 2,
-  name = "かめと",
+  question_id = 2,
+  name = "kameto",
   valid = 0;
 
 INSERT INTO choices
 SET
   id = 5,
-  questions_id = 2,
-  name = "かめど",
+  question_id = 2,
+  name = "kamedo",
   valid = 0;
 
 INSERT INTO choices
 SET
   id = 6,
-  questions_id = 2,
-  name = "かめいど",
+  question_id = 2,
+  name = "kameido",
   valid = 1;
 
 INSERT INTO choices
 SET
   id = 7,
-  questions_id = 3,
-  name = "むこうひら",
+  question_id = 3,
+  name = "mukouhira",
   valid = 0;
 
 INSERT INTO choices
 SET
   id = 8,
-  questions_id = 3,
-  name = "むきひら",
+  question_id = 3,
+  name = "mukihira",
   valid = 0;
 
 INSERT INTO choices
 SET
   id = 9,
-  questions_id = 3,
-  name = "むかいなだ",
+  question_id = 3,
+  name = "mukainada",
   valid = 1;
--- src/index.php
+++ src/index.php
@@ -8,10 +8,12 @@ $big_question_id = $_GET['id'];
 $stmt_big_question_title = $db->prepare('SELECT name FROM big_questions WHERE id = ?');
 $stmt_big_question_title->execute(array($big_question_id));
 $big_question_title = $stmt_big_question_title->fetch();
+
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,16 +33,125 @@ $big_question_title = $stmt_big_question_title->fetch();
         </div>
     </header>
     <main>
+        <h1 class="question_title">
+            <?= $big_question_title['name'] ?>
+        </h1>
+
         <div class="main_container">
-            <h1 class="question_title">
-                <?= $big_question_title['name'] ?>
-            </h1>
             <div class="main_inner" id="quizDivWrapper">
+
+                <?php
+                $question_number = 1;
+                $choice_number = 0;
+                ?>
+
+                <!-- １問目 -->
+                <section class="quiz_div">
+                    <h2 class="question">
+                        <?= $question_number ?>.この地名はなんて読む？
+                    </h2>
+                    <?php
+                    // 設問テーブルから写真のファイル名を持ってくる
+                    $stmt_question_images = $db->prepare('SELECT image FROM questions WHERE id = ?');
+                    $stmt_question_images->execute(array($question_number));
+                    $question_images = $stmt_question_images->fetch();
+                    ?>
+                    <div class="question-image-container">
+                        <img src="./images/<?php echo $question_images['image']; ?>" alt="" class="question_image">
+                    </div>
+
+                    <ul class="option_list" id="optionList3">
+
+                        <?php
+                        for ($i = 0; $i < 3; $i++) {
+
+                            $choice_number++;
+
+                            // 選択肢テーブルから
+                            $stmt_choice_name = $db->prepare('SELECT name FROM choices WHERE id = ?');
+                            $stmt_choice_name->execute(array($choice_number));
+                            $choices_name = $stmt_choice_name->fetch();
+
+                            // 選択肢を表示
+
+                            echo '<li class="option">' . $choices_name['name'] . '</li>';
+                        }
+                        ?>
+
+                        <!-- if文でvalidかどうか調べる -->
+                        <!-- <div class="hidden_message_correct" id="correctPopQ3">
+                            <p class="correct">正解！</p>
+                            <p>正解は<?= '高輪'; ?>です！</p>
+                        </div>
+                        <div class="hidden_message_wrong" id="wrongPopQ3">
+                            <p class="wrong">不正解！</p>
+                            <p>正解は<?= '高輪'; ?>です！</p>
+                        </div> -->
+                        <!-- if文でvalidかどうか調べる 終わり -->
+
+                    </ul>
+                </section>
+
+                <!-- 2問目 -->
+                <section class="quiz_div">
+                    <?php
+                    $question_number++;
+                    ?>
+                    <h2 class="question">
+                        <?= $question_number ?>.この地名はなんて読む？
+                    </h2>
+                    <?php
+                    // 設問テーブルから写真のファイル名を持ってくる
+                    $stmt_question_images = $db->prepare('SELECT image FROM questions WHERE id = ?');
+                    $stmt_question_images->execute(array($question_number));
+                    $question_images = $stmt_question_images->fetch();
+                    ?>
+                    <div class="question-image-container">
+                        <img src="./images/<?php echo $question_images['image']; ?>" alt="" class="question_image">
+                    </div>
+                    <div class="question-image-container">
+                        <img src="./images/<?php echo $question_images[$question_number - 1]; ?>" alt="" class="question_image">
+                    </div>
+
+                    <ul class="option_list" id="optionList3">
+
+                        <?php
+                        for ($i = 0; $i < 3; $i++) {
+
+                            $choice_number++;
+
+                            // 選択肢テーブルから
+                            $stmt_choice_name = $db->prepare('SELECT name FROM choices WHERE id = ?');
+                            $stmt_choice_name->execute(array($choice_number));
+                            $choices_name = $stmt_choice_name->fetch();
+
+                            // 選択肢を表示
+
+                            echo '<li class="option">' . $choices_name['name'] . '</li>';
+                        }
+                        ?>
+
+                        <!-- if文でvalidかどうか調べる -->
+                        <!-- <div class="hidden_message_correct" id="correctPopQ3">
+                            <p class="correct">正解！</p>
+                            <p>正解は<?= '高輪'; ?>です！</p>
+                        </div>
+                        <div class="hidden_message_wrong" id="wrongPopQ3">
+                            <p class="wrong">不正解！</p>
+                            <p>正解は<?= '高輪'; ?>です！</p>
+                        </div> -->
+                        <!-- if文でvalidかどうか調べる 終わり -->
+
+                    </ul>
+                </section>
+
+
             </div>
+
         </div>
         </div>
     </main>
-    <script src="js/kuizy.js"></script>
+    <!-- <script src="js/kuizy.js"></script> -->
 </body>
 
 </html>
